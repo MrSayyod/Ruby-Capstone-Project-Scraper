8b6b20b2bcea7fbe45ff127b2ffb0e8dc03b675c
MrSayyod <ssayyod@gmail.com>
2020-05-20 11:27:18 +0500

    finished scraper with looping

--- bin/main.rb
+++ bin/main.rb
@@ -1,1 +1,4 @@
-#! usr/bin/env ruby
+#! /usr/bin/env ruby
+require_relative '../lib/scraper.rb'
+# puts 'Hello'
+scraper = Scraper.new
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,31 +1,66 @@
 require 'nokogiri'
 require 'open-uri'
-require 'HTTParty'
 require 'byebug'
-# require 'watir'
-# require 'webdrivers'
-
-# args = ['--no-sandbox', '--disable-dev-shm-usage']
-# browser = Watir::Browser.new :chrome, options: {args: args}
-# browser = Watir::Browser.new
-# browser.goto 'https://github.com/MrSayyod'
 class Scraper
-  # def initialize(link)
-  #   @link = link
-  # end 
-  attr_accessor :parse_page 
+  attr_reader :parsed_page
   def initialize
-    url = HTTParty.get("https://olcha.uz/category/telefony-gadzhety-aksessuary/telefony")
-    @parse_page ||= Nokogiri::HTML(url)    
+    url = 'https://www.simplyhired.com/search?q=junior+web+developer&l=&job=qv7hkzh7TcZD8BEHuKv9Eguc_mML6R1OujyCtaMQug7JaEnfmK10kA'
+    doc = ::OpenURI.open_uri(url)
+    html_raw = doc.read
+    @parsed_page = Nokogiri::HTML(html_raw)
+    jobs_list = parsed_page.css('div.SerpJob-jobCard')
+    start(parsed_page, jobs_list)
   end
-  def names
-    smartphones = parse_page.css('div.product__infowrap relative').css('p.product__name')
+
+  private
+
+  def start(parsed_page = nil, jobs_list = nil)
+    page = 1
+    per_page = jobs_list.count
+    total = parsed_page.css('span.CategoryPath-total').text.gsub(',', '').to_i
+    $last_page = (total.to_f / per_page.to_f).ceil
+    sample = Loop.new(total, page)
+    sample.scrapper
   end
-  def price
-    prices = parse_page.css('div.product__pricewrap relative').css('p.product__price')
-  end
-byebug
 end
-scraper = Scraper.new
-names = scraper.names
-price = scraper.price
+
+class Loop < Scraper
+  attr_reader :total, :page
+  def initialize(total, page)
+    @total = total
+    @page = page
+    puts "Overall #{@total}  jobs in #{$last_page} pages"
+  end
+
+  def start
+    iteration_url = "https://www.simplyhired.com/search?q=junior+web+developer&pn=#{page}"
+    sleep(1)
+    puts "Page Number: #{@page}"
+    puts ''
+    iteration_doc = ::OpenURI.open_uri(iteration_url)
+    iteration_unparsed_page = iteration_doc.read
+    iteration_parsed_page = Nokogiri::HTML(iteration_unparsed_page)
+    iteration_parsed_page.css('div.SerpJob-jobCard')
+  end
+
+  def scrapper
+    while @page <= 10
+      iteration_jobs_list = start
+      iteration_jobs_list.each do |x|
+        jobs = { position: x.css('div.jobposting-title-container').css('a.card-link').text,
+                 url: x.css('div.jobposting-title-container').css('a')[0].attributes['href'].value,
+                 location: x.css('h3.jobposting-subtitle').css('span.jobposting-location').text,
+                 company: x.css('h3.jobposting-subtitle').css('span.jobposting-company').text,
+                 salary: x.css('div.SerpJob-metaInfo').css('span.jobposting-salary').text.delete_prefix!('Estimated: ') }
+        puts 'Found jobs'
+        puts "Position: #{jobs[:position]} \n Company: #{jobs[:company]} \n Location: #{jobs[:location]} \n Salary: #{jobs[:salary]} \n Job link: https://www.simplyhired.com#{jobs[:url]}"
+        sleep(0.1)
+      end
+      @page += 1
+      sleep(0.5)
+    end
+    byebug
+  end
+end
+
+
