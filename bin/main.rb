2fc682bdfb51482f0f61397ae111c329f9233315
MrSayyod <ssayyod@gmail.com>
2020-05-22 08:26:00 +0500

    Fixes linter error

--- bin/main.rb
+++ bin/main.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env ruby
+#!/usr/bin/env ruby #rubocop:disable Lint/ScriptPermission
 require_relative '../lib/loop.rb'
 scraper = Scraper.new('https://www.simplyhired.com/search?q=junior+web+developer&l=&job=qv7hkzh7TcZD8BEHuKv9Eguc_mML6R1OujyCtaMQug7JaEnfmK10kA')
 scraper.start
